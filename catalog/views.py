47775917f95d6980dce06ea810e95f559825fcd3
Daniel_MRG <dany.mom2015@gmail.com>
2020-11-17 20:18:04 +0330

    Update views.py
    error for "," this

--- catalog/views.py
+++ catalog/views.py
@@ -36,7 +36,7 @@ class BookDetailView(generic.DetailView):
 # Class Base
 
 class Index(generic.TemplateView):
-    template_name = 'catalog/index.html',
+    template_name = 'catalog/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
